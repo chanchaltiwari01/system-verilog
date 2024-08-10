8e0cb30aaea173e458aa493e85650e5a11ad6b49
chanchaltiwari0101 <chanchaltiwari014501@gmail.com>
2024-08-10 16:24:04 +0530

    enhance

---
+++ enhance1.sv
@@ -0,0 +1,15 @@
+module print();
+rag a = 1 , b=0 ;
+initial begin
+$display("a:%0d,b:%0d",a,b);
+end 
+initial begin
+    $display("first change ");
+end
+initial begin
+    $display("version 5 ");
+end
+initial begin
+    $display("version 6");
+end
+endmodule 
