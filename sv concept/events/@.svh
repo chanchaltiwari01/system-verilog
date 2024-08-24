72c0a7308aa4611cbeed5575fb7eb348e7528f8b
Chanchal Tiwari <147346816+chanchaltiwari01@users.noreply.github.com>
2024-08-25 01:29:15 +0530

    Update @.svh

--- sv concept/events/@.svh
+++ sv concept/events/@.svh
@@ -48,3 +48,28 @@ module events_ex;
     $finish;
   end
 endmodule
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+module tb;
+  // Create an event variable that processes can use to trigger and wait
+  event event_a;
+
+  // Thread1: Triggers the event using "->" operator at 20ns
+  initial begin
+    #20 -> event_a;
+    $display ("[%0t] Thread1: triggered event_a", $time);
+  end
+
+  // Thread2: Starts waiting for the event using "@" operator at 20ns
+  initial begin
+    $display ("[%0t] Thread2: waiting for trigger ", $time);
+     @(event_a);
+    $display ("[%0t] Thread2: received event_a trigger ", $time);
+  end
+
+  // Thread3: Starts waiting for the event using "wait" at 20ns
+  initial begin
+    $display ("[%0t] Thread3: waiting for trigger ", $time);
+    wait (event_a.triggered);
+    $display ("[%0t] Thread3: received event_a trigger", $time);
+  end
+endmodule
