90d4e4a2fc6ba81a9769cd9e8f42867b7cb96610
Chanchal Tiwari <147346816+chanchaltiwari01@users.noreply.github.com>
2024-08-25 01:20:42 +0530

    Update @.svh

--- sv concept/events/@.svh
+++ sv concept/events/@.svh
@@ -19,3 +19,32 @@ module events_ex;
     join
   end
 endmodule
+/////////////////////////////////////////////////////////////////////////////////////////////
+module events_ex;
+
+  event ev_1; //declaring event ev_1
+
+  initial begin
+    fork
+      //process-1, triggers the event
+      begin
+        #40;
+        $display($time,"\tTriggering The Event");
+        ->ev_1;
+      end
+    
+      //process-2, wait for the event to trigger
+      begin
+        $display($time,"\tWaiting for the Event to trigger");
+        #60;
+        @(ev_1.triggered);
+        $display($time,"\tEvent triggered");
+      end
+    join
+  end
+  initial begin
+    #100;
+    $display($time,"\tEnding the Simulation");
+    $finish;
+  end
+endmodule
