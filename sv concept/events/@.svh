b64b32ea4ae4d1422e2bc706c04dbc27685f262f
Chanchal Tiwari <147346816+chanchaltiwari01@users.noreply.github.com>
2024-08-25 01:10:30 +0530

    Create @.svh

---
+++ sv concept/events/@.svh
@@ -0,0 +1,21 @@
+module events_ex;
+  event ev_1; //declaring event ev_1
+
+  initial begin
+    fork
+      //process-1, triggers the event
+      begin
+        #40;
+        $display($time,"\tTriggering The Event");
+        ->ev_1;
+      end
+    
+      //process-2, wait for the event to trigger
+      begin
+        $display($time,"\tWaiting for the Event to trigger");
+        @(ev_1.triggered);
+        $display($time,"\tEvent triggered");
+      end
+    join
+  end
+endmodule
