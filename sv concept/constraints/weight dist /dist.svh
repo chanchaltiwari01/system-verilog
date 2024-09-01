a038efe9e851d2b24363e2cd5d35d8f6b0e97f79
Chanchal Tiwari <147346816+chanchaltiwari01@users.noreply.github.com>
2024-09-01 22:43:16 +0530

    Update dist.svh

--- sv concept/constraints/weight dist /dist.svh
+++ sv concept/constraints/weight dist /dist.svh
@@ -9,13 +9,42 @@ class packet ;
 endclass 
 
 module test ;
+  int count ;
   packet pkt ;
   initial begin
     pkt = new();
     repeat(10) begin
       pkt.randomize();
       $display("data = %0d",pkt.data);
+      if(pkt.data == 6) begin
+        count = count + 1;
+        $display(count);
+        end 
       #1;
     end 
   end 
+endmodule 
+//////////////////////////////////////////////////////////////////////////
+class packet ;
+  rand int data ;
+  constraint con { data dist{0:/20 , [1:3]:/60, [6:7]:/20};}
+  // total weight = 20 + 60 + 40 = 100.
+  // data = 0  weight = 20/100 = 20%;
+  // data = 1 weight = 20/100 = 20%.
+  // data = 6 weight = 10/100 = 10%
+  
+endclass 
+
+module test;
+  
+  packet pkt ;
+  
+  initial begin
+    pkt = new();
+    repeat(10) begin
+      pkt.randomize();
+      $display("data=%0d",pkt.data);
+      #1;
+    end
+  end
 endmodule 
